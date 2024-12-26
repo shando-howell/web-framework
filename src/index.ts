ab5f39c8a83087f380c034799a724c38b5042c54
shandohowell <shandohowell94@gmail.com>
2024-12-26 14:08:25 -0500

    Worked on sync and event system.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { User } from "./models/User";
 
 const user = new User({ id: 1 });
-
-user.fetch();
-
-setTimeout(() => {
-    console.log(user);
-}, 4000)
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
 interface UserProps {
     id?: number;
@@ -6,12 +6,10 @@ interface UserProps {
     age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-    constructor(private data: UserProps) {}
+    public events: Eventing = new Eventing();
 
-    events = {}
+    constructor(private data: UserProps) {}
 
     get(propName: string): (number | string) {
         return this.data[propName];
@@ -19,31 +17,5 @@ export class User {
 
     set(update: UserProps): void {
         Object.assign(this.data, update);
-    }
-
-    on(eventName: string, callback: Callback): void {
-        const handlers = this.events[eventName] || [];
-        handlers.push(callback);
-        this.events[eventName] = handlers;
-    }
-
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName];
-
-        if (!handlers || handlers.length === 0) {
-            return;
-        }
-
-        handlers.forEach(callback => {
-            callback();
-        });
-    }
-
-    fetch(): void {
-        axios.get(`http://localhost:3000/users/${this.get('id')}`)
-            .then((response: AxiosResponse): void => {
-                this.set(response.data);
-            }
-        );
     }
 }
