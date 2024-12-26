bf5b158c836053a9207394ae9656fbf5ded2eca7
shandohowell <shandohowell94@gmail.com>
2024-12-26 15:37:04 -0500

    Worked on the attributes system.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,18 @@
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
-interface UserProps {
+export interface UserProps {
     id?: number;
     name?: string;
     age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
     public events: Eventing = new Eventing();
-
-    constructor(private data: UserProps) {}
-
-    get(propName: string): (number | string) {
-        return this.data[propName];
-    }
-
-    set(update: UserProps): void {
-        Object.assign(this.data, update);
-    }
+    public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+    // Pick up here...
+    // public attributes: Attributes<UserProps> = new Attributes<UserProps>();
 }
